51f050f67d3ca203c4bb24126c14a1a7dc587081
gerryRcom <gerryr@gerryr.com>
2021-03-12 22:22:50 +0000

    working draft code to transfer to testing

--- cc.check.py
+++ cc.check.py
@@ -10,9 +10,10 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-# import subprocess to allow running of shell commands
+# imports to allow running of shell commands and regex
 import subprocess
 import sys
+import re
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
@@ -46,16 +47,22 @@ def main():
   service = build('sheets', 'v4', credentials=creds)
 
   # query type a (replace with cnc)
-  def queryip(ipinput):
-    ipresult = subprocess.run(['nslookup', ipinput], capture_output=True, text=True)
-    if (ipresult.stderr != ""):
-      return ipresult.stderr
+  def queryname(querynameinput):
+    nameresult = subprocess.run(['nslookup', querynameinput], capture_output=True, text=True)
+    if (nameresult.stderr != ""):
+      return nameresult.stderr
     else:
-      return ipresult.stdout
+      return nameresult.stdout
 
   # query type b (replace with rig)
 
   # query type c (replace with net)
+  def queryip(queryipinput):
+    ipresult = subprocess.run(['ping', '-c 2', queryipinput], capture_output=True, text=True)
+    if (ipresult.stderr != ""):
+      return ipresult.stderr
+    else:
+      return ipresult.stdout
 
   # Call the Sheets API
   sheet = service.spreadsheets()
@@ -63,28 +70,38 @@ def main():
                               range=CC_RANGE_NAME).execute()
   values = result.get('values', [])
 
-  # keep track of row number for reporting
-  rowNum=1
-
   if not values:
     print('No data found in sheet (check cc tab).')
-  else:      
+  else:
+    # keep track of row number for reporting issues
+    rowNum=1
+    ip4Regex = re.compile(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
     for row in values:
       # check for missing entries
       if (row[0] == "" or row[1] == ""):
         print("Missing data on row: " + str(rowNum))
         rowNum+=1
-      # run a naming check on the entries
-      elif (row[0][-4:] != ".com" or row[1][-4:] != ".com"):
+      # run a fqdn naming check on the entries (alternative .endswith)
+      elif (row[0][-4:] != ".com" or not row[1].endswith(".com")):
         print("Invalid data on row: " + str(rowNum))
         rowNum+=1
       else:
-        output = queryip(row[0])
-        for line in output.splitlines():
-          if ('Server' in line):
-            print(line)
-          elif ('Address' in line):
-            print(line.split()[-1])    
+        # run name query against entry
+        querynameoutput = queryname(row[0])
+        for querynameline in querynameoutput.splitlines():
+          if ('Server' in querynameline):
+            print(querynameline)
+          # If Address is returnned check last item in the line, should be an IP address.
+          elif ('Address' in querynameline):
+            # send valid IP to IP check
+            if (ip4Regex.match(querynameline.split()[-1])):
+              queryipoutput = queryip(querynameline.split()[-1])
+              # return specific lines in ip check
+              for queryipline in queryipoutput.splitlines():
+                if ('bytes' in queryipline):
+                  print(queryipline)
+            else:
+              print ("Not a valid ipv4 address %s" % querynameline.split()[-1])
         rowNum+=1
 
 if __name__ == '__main__':
